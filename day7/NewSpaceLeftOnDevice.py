b6a1b1894a649283f656aa45b3330adedb7ac248
izurza <izurza1994@gmail.com>
2022-12-08 15:09:54 +0100

    Seventh Commit

---
+++ day7/NewSpaceLeftOnDevice.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+#learned from Jonathan Paulson
+
+input = open("day7/inputtest.txt", "r")
+# input = open("day7/input.txt", "r")
+cl = input.read().split("\n")
+SZ=defaultdict(int)
+path=[]
+for l in cl:
+    words = l.strip().split()
+    if words[1]=="cd":#moving
+        if words[2]=="..": #parent folder
+            path.pop()
+        else : 
+            path.append(words[2]) #next folder
+    elif words[1]=="ls": #do nothing
+        continue
+    elif words[0]=="dir": #do nothing
+        continue
+    else: #get the value of the files and sum them to the path
+        sz=int(words[0])
+        for i in range(len(path)+1):
+            SZ['/'.join(path[:i])]+=sz
+
+#conditions for part 2
+max_used =70000000-30000000
+total_used = SZ['/']
+need_to_free = total_used - max_used
+best = 1e9
+
+ans=0
+for k,v in SZ.items():
+    print(k)
+    if v<=100000:#get the values of the directories that have size no more than 100000
+        ans+=v 
+    if v>=need_to_free: #get the value of the directory with the minimum size, that lets you update
+        best=min(best,v)
+print(ans)
+print(best)
