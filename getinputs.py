3cf8138cb252f352631aef1c0a153fe3c24c9718
i.izurza <i.izurza@consulpyme.com>
2023-12-11 18:06:30 +0100

    refacatoring

--- getinputs.py
+++ getinputs.py
@@ -3,14 +3,15 @@ from pathlib import Path
 
 def get_input(day=1, year=2023):
     cookies = {
-        'session': '<COOKIE-VALUE>'
+        'session': 'cookie'
     }
     r = requests.post(f'https://adventofcode.com/{year}/day/{day}/input', cookies=cookies)
 
-    p = Path(f'{year}/day{day}')
+    p = Path(f'{year}/inputs/')
     p.mkdir(parents=True, exist_ok=True)
 
     with open(f'{p}/day{day}.in','w+') as f:
         f.write(r.text)
 
 get_input()
+
